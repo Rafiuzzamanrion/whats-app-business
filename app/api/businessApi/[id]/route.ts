ceba078ddaec419b231668143236bd941bd3f34e
Rafiuzzaman Rion <rafiujjaman5683@gmail.com>
2025-07-17 16:05:50 +0600

    fixed order by id

--- app/api/businessApi/[id]/route.ts
+++ app/api/businessApi/[id]/route.ts
@@ -1,39 +1,28 @@
 import { NextResponse } from "next/server";
-import { getServerSession } from "next-auth";
-import { Role } from "@prisma/client";
 
-import { authOptions } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
 
 export async function GET(
   request: Request,
   { params }: { params: { id: string } },
 ) {
-  const session = await getServerSession(authOptions);
-
-  if (
-    !session ||
-    (session.user.role !== Role.ADMIN && session.user.role !== Role.SUPER_ADMIN)
-  ) {
-    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-  }
-  const { id } = params;
-
   try {
-    const businessApi = await prisma.businessApi.findUnique({
-      where: { id },
+    const business = await prisma.businessApi.findUnique({
+      where: {
+        id: params.id,
+      },
     });
 
-    if (!businessApi) {
+    if (!business) {
       return NextResponse.json(
-        { error: "Business API not found" },
+        { error: "Business not found" },
         { status: 404 },
       );
     }
 
-    return NextResponse.json(businessApi);
+    return NextResponse.json(business);
   } catch (error) {
-    console.error("Error:", error);
+    console.error("Error fetching business:", error);
 
     return NextResponse.json(
       { error: "Internal server error" },
