889dd1433e918fb08d95c805e9ed7101e63638fc
Rafiuzzaman Rion <rafiujjaman5683@gmail.com>
2025-07-27 20:13:53 +0600

    completed dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -171,6 +171,7 @@ const Page = () => {
               size="lg"
               src={"user.png"}
             />
+
             <div>
               <h1 className="text-3xl font-bold bg-gradient-to-r from-green-600 to-green-800 bg-clip-text text-transparent">
                 My Dashboard
